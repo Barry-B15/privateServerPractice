c18b47b5ab2eca7c572870fc97f9391e1aa89c0c
Barry <Barry-B15@users.noreply.github.com>
2020-03-27 14:39:40 +0900

    Use GET and find() function to query db

--- index.js
+++ index.js
@@ -2,14 +2,11 @@
 const express = require('express');
 
 const Datastore = require('nedb'); // require nedb SD 1
-const app = express(); //2. start an instance of app
 
-//3. init the main project folder to use static site
-// the folder is here named 'website' can be a diff name
-// create an index.html file inside the website dir
+const app = express(); //2. start an instance of app
+const port = 8080;
 app.use(express.static('website'));
 
-//8. tell server to use json before logging the request.body, add a limit to the data that can be received at a go 
 // see: https://expressjs.com/ja/api.html
 app.use(express.json({ limit: '1mb' }));
 
@@ -18,20 +15,32 @@ app.use(express.json({ limit: '1mb' }));
 const database = new Datastore('database.db'); // create the db SD 2
 database.loadDatabase(); // create the db locally SD 3
 //now insert data into the db SD4
+/* 
 database.insert({ name: 'Gbene Zini', status: 'surfing' });
 database.insert({ name: 'The Boss', status: 'training' });
+ */
 
-
-//4. add a port for the server to run on
-// can 3000, 3030, 8080, --
-const port = 8080;
 
 app.listen(port, () => console.log(`Listening at localhost: ${port}`));
+
+// create a get request to handle GET 
+app.get('/api', (request, response) => {
+    //response.json({ test: 123 }); // lets just test if it works 1st
+
+    //Now let's use the find() func to get the actual data
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+        response.json(data);
+    });
+});
 
 //7. POST request to send data from client
 app.post('/api', (request, response) => {
     //console.log(request); // 1st log the request and see
-    console.log('This is the request data');
+    //console.log('This is the request data');
     //console.log(request.body); 
 
     const data = request.body; //this + data.
@@ -47,10 +56,13 @@ app.post('/api', (request, response) => {
 
     database.insert(data); // insert user data to db SD5
     //console.log(database) // instead of logging body add the timestamp to the response.json
-    response.json({
+
+    /* response.json({
         status: 'success',
         timestamp: timestamp, // add the timestamp to what we r sending back to the client SD6b
         latitude: data.lat, //request.body.lat,
         longitude: data.lon //request.body.lon //
-    });
+    }); */
+    // replace the above with a shorter one
+    response.json(data);
 });
--- website/js/app.js
+++ website/js/app.js
@@ -4,9 +4,16 @@ const button = document.getElementById('submit'); //def a constant button
 // add a click listener to the btn
 button.addEventListener('click', async event => {
     // move the data an header into the listener
+
+    //def a new const for vegetable
+    const vegetable = document.getElementById('vegetable').value;
+    const mood = document.getElementById('mood').value;
+
     const data = {
         lat,
-        lon
+        lon,
+        vegetable, // add the veg to data
+        mood
     }
     const options = {
         method: 'POST',
@@ -20,6 +27,46 @@ button.addEventListener('click', async event => {
     const json = await response.json();
     console.log(json);
 });
+
+//==============GET=======================
+getData(); // call the func we just created
+
+//fetch('/api'); // fetch the data
+//lets put our fetch() in an async func and store the result as response
+/* async function getData() {
+    const response = await fetch('/api');
+    const data = await response.json();
+    console.log(data); // then call this func above it
+} */
+// How about publishing this to the page!
+// We can rewrite the above code as
+async function getData() {
+    const response = await fetch('/api');
+    const data = await response.json();
+
+    for (item of data) {
+        const root = document.createElement('div');
+        const mood = document.createElement('div');
+        const vegetable = document.createElement('div');
+        const geo = document.createElement('div');
+        const date = document.createElement('div');
+
+        mood.textContent = `Mood: ${item.mood}`;
+        vegetable.textContent = `Favorite Veggie: ${item.vegetable}`;
+        geo.textContent = `Lat: ${item.lat}°, Lon: ${item.lon}°`;
+        const dateString = new Date(item.timestamp).toLocaleString();
+        date.textContent = dateString;
+
+        root.append(mood, vegetable, geo, date);
+        document.body.append(root);
+
+    }
+    console.log(data); // then call this func above it
+}
+
+
+//======================END GET
+
 //Remake the if sentence
 if ('geolocation' in navigator) {
     /* geolocation is available */
